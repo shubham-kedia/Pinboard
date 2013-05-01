b230da376faed2ceddf0194f31b794e9b9df7b38
Senthil <psenthil202@gmail.com>
2013-05-01 22:30:29 +0530

    Capified url : pinboard.codingmart.com

---
+++ config/deploy.rb
@@ -0,0 +1,12 @@
+set :application, 'Pinboard'
+set :scm, :git
+set :repository, "git@github.com:pSenthil202/pinboard.git"
+set :user, "root"
+set :scm_passphrase, ""
+set :branch, "master"
+set :deploy_via, :remote_cache
+require 'capistrano/ext/multistage'
+set :stages, ["staging", "development", "production"]
+set :default_stage, "development"
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
