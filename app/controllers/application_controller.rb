b2ac8e68f8db0f23b8054a1939aa8f905f9bd29f
marudhupandiyang <marudhupandiyang@gmail.com>
2013-05-03 12:36:32 +0530

    Header, login_check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def require_login
-   if !current_user
+   if !user_signed_in?
 			redirect_to "/", :flash => { :notice => "Please Login" }
 		end
   end
