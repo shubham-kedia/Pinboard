07fa34af1f71bdf305b77748f3fe44b3a3595719
marudhupandiyang <marudhupandiyang@gmail.com>
2013-05-06 15:26:25 +0530

    Cancel button for settings, note created twice...\!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,11 +6,10 @@
 //
 //= require jquery
 //= require jquery_ujs
-
 //= require jquery.ui.all
-
 //= require twitter/bootstrap
 //= require_tree .
+
 $(document).ready(function(){
    window.notice=function(header,body){
       $('#notice_header').html(header);
