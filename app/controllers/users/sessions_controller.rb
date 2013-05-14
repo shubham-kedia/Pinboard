98aa7861090982f979a877f2ec372c91f512c883
balachandar2k12 <bala@devbrother.com>
2013-05-14 15:17:30 +0530

    login correction

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,14 +1,16 @@
 class Users::SessionsController < Devise::SessionsController
+  def new
+    super
+  end
   def create
-     resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#new")
-     team_id = Team.find(params[:user][:team_id])
-     if team_id == resource.team_id
-     	sign_in(resource_name, resource)
-			else
-			 redirect_to "/"
-     end
-
-
+      team_id = Team.find(params[:user][:team_id]).id
+      user = User.find_by_email(params[:user][:email])
+      if team_id == user.team_id
+        resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#new")
+     	  sign_in_and_redirect(resource_name, resource)
+      else
+        redirect_to root_url
+		  end
      #respond_with resource, :location => redirect_location(resource_name, resource)
    end
 end
